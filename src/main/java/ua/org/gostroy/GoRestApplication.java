c87efa8a4bbae2f0dbf65f941cb27fdf34ee3bb2
panser <gostroy@gmail.com>
2016-04-06 15:49:50 +0300

    init spring-boot project with monitoring futures http://localhost:8080/actuator/

---
+++ src/main/java/ua/org/gostroy/GoRestApplication.java
@@ -0,0 +1,12 @@
+package ua.org.gostroy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoRestApplication.class, args);
+    }
+}
